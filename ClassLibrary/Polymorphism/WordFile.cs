c281208bcb9f592f63fb55645a1ebd00d4b0fdad
vigorjao <vigor.jao@eitc.com.tw>
2016-12-18 23:24:01 +0800

    萬能加載器

---
+++ ClassLibrary/Polymorphism/WordFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary.Polymorphism
+{
+    public class WordFile : DocFile
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Open the Word file.");
+        }
+    }
+}
